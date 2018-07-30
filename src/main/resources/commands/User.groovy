6d904a12f103c5df5a7da3b26fd62f2c1108737a
Nik <nik@monsterlyrics.co>
2018-07-30 13:58:19 -0400

    Improved date parsing and added checks to make sure the date is not in the future

--- src/main/resources/commands/User.groovy
+++ src/main/resources/commands/User.groovy
@@ -1,6 +1,7 @@
 package commands
 
 import co.vulpin.birthday.db.Database
+import co.vulpin.birthday.db.entities.User as DbUser
 import co.vulpin.commando.CommandEvent
 import co.vulpin.commando.annotations.Aliases
 import co.vulpin.commando.annotations.Cmd
@@ -9,10 +10,11 @@ import com.google.cloud.firestore.DocumentReference
 import com.google.cloud.firestore.SetOptions
 import decorators.BasicPerms
 
+import java.time.LocalDate
+import java.time.LocalTime
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
-import co.vulpin.birthday.db.entities.User as DbUser
 
 @Aliases(["me"])
 @Optional
@@ -22,51 +24,28 @@ class User {
 
     @Cmd
     @BasicPerms
-    void set(CommandEvent event, String dayString, String monthString, String yearString, String gmtOffsetString) {
+    void set(CommandEvent event, String day, String month, String year, String gmtOffset) {
         def ref = Database.instance.getUserRef(event.author.id)
 
-        if(!dayString.isInteger()) {
-            event.replyError("$dayString is not a valid day!").queue()
-            return
-        }
-
-        if(!monthString.isInteger()) {
-            event.replyError("$monthString is not a valid month!").queue()
-            return
-        }
-
-        if(!yearString.isInteger()) {
-            event.replyError("$yearString is not a valid year!").queue()
-            return
-        }
-
-        if(!dayString.isInteger()) {
-            event.replyError("$gmtOffsetString is not a valid GMT offset!").queue()
-            return
-        }
-
-        int day = dayString as int
-        int month = monthString as int
-        int year = yearString as int
-        int gmtOffset = gmtOffsetString as int
-
-        def dateTime
+        def date
         try {
-            def offset = ZoneOffset.ofHours(gmtOffset)
-            dateTime = OffsetDateTime.of(year, month, day, 0, 0 ,0 ,0, offset)
+            date = parseDate(day, month, year, gmtOffset)
         } catch (e) {
-            event.replyError("An error occurred while parsing your birthday!", e.message).queue()
+            event.replyError(
+                "An error occurred while parsing that date!",
+                "**$e.class.simpleName:** $e.message"
+            ).queue()
             return
         }
 
         ref.set([
-                birthDay: day,
-                birthMonth: month,
-                birthYear: year,
-                gmtOffset: gmtOffset
+                birthDay: date.dayOfMonth,
+                birthMonth: date.monthValue,
+                birthYear: date.year,
+                gmtOffset: date.offset.totalSeconds / 60 / 60 as int
         ], SetOptions.merge())
 
-        event.reply("Successfully set up your birthday to **${dateTime.format(dateFormatter)}**!").queue()
+        event.reply("Successfully set up your birthday to **${date.format(dateFormatter)}**!").queue()
     }
 
     @Cmd
@@ -95,6 +74,25 @@ class User {
     void remove(CommandEvent event) {
         getUserRef(event).delete()
         event.reply("Your birthday has been removed :wave: Sorry to see you go :pensive:").queue()
+    }
+
+    private OffsetDateTime parseDate(String day, String month, String year, String gmtOffset) {
+        return parseDate(day as int, month as int, year as int, gmtOffset as int)
+    }
+
+    private OffsetDateTime parseDate(int day, int month, int year, int gmtOffset) {
+        def date = LocalDate.of(year, month, day)
+        def time = LocalTime.of(0, 0)
+        def zone = ZoneOffset.ofHours(gmtOffset)
+
+        def birthday = OffsetDateTime.of(date, time, zone)
+
+        def now = OffsetDateTime.now()
+
+        if(birthday.isAfter(now))
+            throw new IllegalArgumentException("The parsed date cannot be in the future!")
+
+        return birthday
     }
 
     private DocumentReference getUserRef(CommandEvent event) {
