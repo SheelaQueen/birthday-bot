461d92d5e5281e55755f956875bf0622477fc3db
Nik <nik@monsterlyrics.co>
2018-07-30 19:16:24 -0400

    Fake commit to trigger new build

--- src/main/groovy/co/vulpin/birthday/db/Database.groovy
+++ src/main/groovy/co/vulpin/birthday/db/Database.groovy
@@ -14,6 +14,7 @@ class Database {
     private Firestore firestore
 
     Database() {
+
         def creds = GoogleCredentials.applicationDefault
 
         def opts = FirestoreOptions.newBuilder()
@@ -22,6 +23,7 @@ class Database {
                 .build()
 
         firestore = opts.service
+
     }
 
     DocumentReference getGuildRef(String guildId) {
