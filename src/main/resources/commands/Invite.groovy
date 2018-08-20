dc82029b667b476c60d2822cdb4ce82e0cc95797
Nik <nik@monsterlyrics.co>
2018-08-20 14:06:03 -0400

    Updated invite command

--- src/main/resources/commands/Invite.groovy
+++ src/main/resources/commands/Invite.groovy
@@ -5,12 +5,7 @@ import co.vulpin.commando.annotations.Cmd
 import co.vulpin.commando.annotations.Optional
 import decorators.BasicPerms
 
-import static net.dv8tion.jda.core.Permission.MANAGE_ROLES
-import static net.dv8tion.jda.core.Permission.MESSAGE_ADD_REACTION
-import static net.dv8tion.jda.core.Permission.MESSAGE_EMBED_LINKS
-import static net.dv8tion.jda.core.Permission.MESSAGE_EXT_EMOJI
-import static net.dv8tion.jda.core.Permission.MESSAGE_READ
-import static net.dv8tion.jda.core.Permission.MESSAGE_WRITE
+import static net.dv8tion.jda.core.Permission.*
 
 class Invite {
 
@@ -18,12 +13,7 @@ class Invite {
     @Optional
     @BasicPerms
     void get(CommandEvent event) {
-        event.reply("[**Invite**](${getInviteUrl(event)})").queue()
-    }
-
-    @Cmd
-    void basic(CommandEvent event) {
-        event.reply(getInviteUrl(event)).queue()
+        event.reply("**[Invite](${getInviteUrl(event)})**").queue()
     }
 
     private String getInviteUrl(CommandEvent event) {
